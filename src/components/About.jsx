f98f1a78289c0df1d0735755eeb5cc64769477f5
SuriTian <suritian9@gmail.com>
2026-01-26 14:01:41 -0500

    about me cloud

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './About.css';
+
+function WordCloud() {
+    const list = [
+        ['Hobbies', 30],
+        ['(Programming) Languages', 70],
+        ['Horror', 20],
+        ['Coursework', 50],
+        ['Movies', 30],
+        ['Leadership', 60],
+        ['Entrepreneurship', 50],
+        ['Music', 30],
+    ];
+
+    return (
+        <div>
+        {list.map(([word, size], idx) => (
+            <button
+                key={idx}
+                style={{
+                    fontSize: `${size}px`
+                }}
+            >
+            {word}
+            </button>
+        ))}
+        </div>
+    );
+
+}
 
 const About = () => {
     return (
         <section className='about' id="about">
             <h1>About Me</h1>
-            <div className='container'>
-                <div className='about-text'>
-                    <h2>Take a look!</h2>
-                </div>
+            <div className='word-cloud'>
+                <WordCloud />
             </div>
         </section>
     );
--- src/components/Fun.jsx
+++ src/components/Fun.jsx
@@ -3,11 +3,13 @@ import { useState } from 'react'
 import './Fun.css'
 import TicTacToe from './TicTacToe'
 
+// , but I decided to extend it to ultimate tic tac toe add an AI to it. Try to win!
+
 const Fun = () => {
     return (
         <section className='fun' id="fun">
             <h1>Fun!!!</h1>
-            <p>Making TicTacToe was my introduction to React, but I decided to extend it to ultimate tic tac toe add an AI to it. Try to win!</p>
+            <p>Making TicTacToe was my introduction to React</p> 
             <TicTacToe/>
         </section>
     );
