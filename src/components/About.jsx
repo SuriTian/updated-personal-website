2f9d0b76a1734060cadb230660ea519eee17265e
SuriTian <suritian9@gmail.com>
2026-01-26 00:07:50 -0500

    try to add an image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './About.css';
+import businessImg from '../assets/Business.JPEG';
 
 const About = () => {
     return (
         <section className='about' id="about">
             <h1>About Me</h1>
-            <div className='section-container'>
+            <div className='container'>
                 <div className='about-text'>
                     <h2>Take a look!</h2>
                 </div>
                 <div className='picture-carousel'>
-                    {/* pictures here later */}
+                    <img className="business-img" src={businessImg}></img>
                 </div>
             </div>
         </section>
